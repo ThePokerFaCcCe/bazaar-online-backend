3cc5663bea4ea1c963a2c2ee440af43d89e4b2d4
ThePokerFaCcCe <ThePokerFa3e@gmail.com>
2023-02-10 00:51:42 +0330

    Change GetCategoryFeatures Response Structure

--- Src/BazaarOnline.Application/ViewModels/Categories/CategoryFeatures/CategoryFeauresListDetailViewModel.cs
+++ Src/BazaarOnline.Application/ViewModels/Categories/CategoryFeatures/CategoryFeauresListDetailViewModel.cs
@@ -1,23 +1,57 @@
-﻿using BazaarOnline.Domain.Entities.Features;
+﻿using BazaarOnline.Application.Utils.Extensions;
+using BazaarOnline.Domain.Entities.Features;
 
 namespace BazaarOnline.Application.ViewModels.Categories.CategoryFeatures;
+
+// Changed for front developer request.
 
 public class CategoryFeaturesListDetailViewModel
 {
     public int Id { get; set; }
 
-    public bool IsRequired { get; set; }
-
-    public CategoryFeaturesListFeatureDetailViewModel Feature { get; set; }
+    public CategoryFeaturesListFeatureDetailViewModel Data { get; set; }
 }
 
 public class CategoryFeaturesListFeatureDetailViewModel
 {
+    public bool IsRequired { get; set; }
+
     public string Name { get; set; }
 
     public string Description { get; set; }
 
     public FeatureTypeEnum Type { get; set; }
 
-    public object TypeObject { get; set; }
+    public string TypeName => Type.GetDisplayName();
+
+    #region StringValidation
+
+    public int? MinLength { get; set; }
+
+    public int? MaxLength { get; set; }
+
+    public string? Regex { get; set; }
+
+    #endregion
+
+    #region IntegerValidation
+
+    public long? Minimum { get; set; }
+
+    public long? Maximum { get; set; }
+
+    #endregion
+
+    #region SelectValidation
+
+    public string? Options
+    {
+        set => OptionsList = value?.Split('\u002C' /* , */);
+    }
+
+    public IEnumerable<string>? OptionsList { get; set; }
+
+    #endregion
+
+    public string Placeholder { get; set; }
 }
